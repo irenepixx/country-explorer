5f80435da84822d3a0ddcad44f77ca4ac78fad88
Irene <irenecallei@gmail.com>
2025-04-23 13:13:50 +0200

    feat: Create route path for favorites

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,9 @@
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Navbar from './components/Navbar';
-import Home from './pages/Home';
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Home from "./pages/Home";
+import Favorites from "./pages/Favorites";
+import { FavoritesProvider } from "./utils/favoritesContext";
 
 const App: React.FC = () => {
   return (
@@ -9,10 +11,17 @@ const App: React.FC = () => {
       <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route
+          path="/favorites"
+          element={
+            <FavoritesProvider>
+              <Favorites />
+            </FavoritesProvider>
+          }
+        />
       </Routes>
     </Router>
   );
 };
 
 export default App;
-
