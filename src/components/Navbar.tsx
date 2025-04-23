4825810503a179d305003d50b7760ea3cdd42a35
Irene <irenecallei@gmail.com>
2025-04-23 11:27:24 +0200

    style: Update logo text from "MyApp" to "Country Explorer"

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,12 +1,14 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React from "react";
+import { Link } from "react-router-dom";
 
 const Navbar: React.FC = () => {
   return (
     <nav style={styles.nav}>
-      <h2 style={styles.logo}>MyApp</h2>
+      <h2 style={styles.logo}>Country Explorer</h2>
       <div>
-        <Link to="/" style={styles.link}>Home</Link>
+        <Link to="/" style={styles.link}>
+          Home
+        </Link>
       </div>
     </nav>
   );
@@ -14,27 +16,26 @@ const Navbar: React.FC = () => {
 
 const styles = {
   nav: {
-    position: 'fixed',
+    position: "fixed",
     top: 0,
     left: 0,
-    width: '100%',
-    padding: '0 20px',
-    backgroundColor: '#333',
-    color: 'white',
-    display: 'flex',
-    justifyContent: 'space-between',
-    alignItems: 'center',
-    zIndex: 1000
+    width: "100%",
+    padding: "0 20px",
+    backgroundColor: "#333",
+    color: "white",
+    display: "flex",
+    justifyContent: "space-between",
+    alignItems: "center",
+    zIndex: 1000,
   } as React.CSSProperties,
   link: {
-    color: 'white',
-    textDecoration: 'none',
-    marginLeft: '15px'
+    color: "white",
+    textDecoration: "none",
+    marginLeft: "15px",
   } as React.CSSProperties,
   logo: {
-    margin: 0
-  } as React.CSSProperties
+    margin: 0,
+  } as React.CSSProperties,
 };
 
 export default Navbar;
-
