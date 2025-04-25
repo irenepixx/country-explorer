fcccd16e3ebd9dd348a3471743bc76271819b04b
Irene <irenecallei@gmail.com>
2025-04-25 11:06:41 +0200

    feat: Define useFavorites hook

--- src/contexts/favoritesContext.tsx
+++ src/contexts/favoritesContext.tsx
@@ -12,7 +12,13 @@ const FavoritesContext = createContext<FavoritesContextType | undefined>(
   undefined
 );
 
-export const useFavorites = () => useContext(FavoritesContext);
+export const useFavorites = () => {
+  const context = useContext(FavoritesContext);
+  if (!context) {
+    throw new Error("useFavorites must be used within a FavoritesProvider");
+  }
+  return context;
+};
 
 export const FavoritesProvider = ({
   children,
