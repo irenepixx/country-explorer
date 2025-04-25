ce7becb30a16823a7f24a99198d59352e61006f9
Irene <irenecallei@gmail.com>
2025-04-25 13:14:20 +0200

    refacture: Restructure Sidebar logic

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,20 @@
-import React, { useState } from "react";
+import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import Home from "./pages/Home";
-import Sidebar from "./components/Sidebar";
 import "./App.css";
 import FavoritesProvider from "./context/FavoritesContext";
 
 const App: React.FC = () => {
-  const [isSidebarOpen, setIsSidebarOpen] = useState(false);
-
   return (
-    <Router>
-      <Navbar onFavoritesClick={() => setIsSidebarOpen(true)} />
-      <Sidebar isOpen={isSidebarOpen} onClose={() => setIsSidebarOpen(false)} />
-      <Routes>
-        <Route path="/" element={<Home />} />
-      </Routes>
-    </Router>
+    <FavoritesProvider>
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </Router>
+    </FavoritesProvider>
   );
 };
 
