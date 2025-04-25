7bf93662797eab0f638e5a75a835d9dfe929ea36
Irene <irenecallei@gmail.com>
2025-04-25 10:39:49 +0200

    style: Add app style

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import Home from "./pages/Home";
 import Sidebar from "./components/Sidebar";
+import "./App.css";
 
 const App: React.FC = () => {
   const [isSidebarOpen, setIsSidebarOpen] = useState(false);
