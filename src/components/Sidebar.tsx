f40ad4c799ee8816abfc87418f3a373455b7a30a
Irene <irenecallei@gmail.com>
2025-04-29 14:33:15 +0200

    feat: Update Sidebar import

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,4 +1,8 @@
-import React from "react";
+import React, { useRef, useEffect } from "react";
+import { Link } from "react-router-dom";
+import { FaTimes } from "react-icons/fa";
+import { Country } from "../types/Country";
+import "./Sidebar.css";
 
 interface SidebarProps {
   isOpen: boolean;
